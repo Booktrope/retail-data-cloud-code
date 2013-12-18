446ee1de0fcaaab7ef69f3247930de183d6ce5cb
Justin Jeffress <jjeffress>
2013-12-17 19:36:53 -0800

    getCrawlDataForAsin Bug Fix
    Parse returns success even if the results of a query were 0. Therefore you must check the number of results
    
    Signed-off-by: Justin Jeffress <jjeffress>

--- cloud/main.js
+++ cloud/main.js
@@ -10,30 +10,41 @@ Parse.Cloud.define("getCrawlDataForAsin", function(request, response){
 	query.equalTo("asin", request.params.asin);
 	query.include("book");
 	//query.descending("crawl_date,crawl_time");
-	query.descending("createdAt");
+	query.descending("crawl_date");
+	if(request.params.skip)
+	{
+		query.skip(request.params.skip)
+	}
 	//query.limit(request.params.limit);
 	query.find({
 		success: function(results){
 			
-			var myBook = results[0].get("book");
-			
 			var payLoad = {};
+			payLoad.title = "";
+			payLoad.author = "";
 			var crawlData = [];
-			payLoad.title = myBook.get("title");
-			payLoad.author = myBook.get("author");
-			for(var i = 0; i < results.length; i++)
+			payLoad.crawl = crawlData;
+			if(results[0] != null)
 			{
-				crawlData[i] = {};
-				crawlData[i].asin           = results[i].get("asin");
-				crawlData[i].sales_rank     = results[i].get("sales_rank");
-				crawlData[i].average_stars  = results[i].get("average_stars");
-				crawlData[i].kindle_price   = results[i].get("kindle_price");
-				crawlData[i].num_of_reviews = results[i].get("num_of_reviews");				
-				crawlData[i].crawl_date     = results[i].get("crawl_date");
-				crawlData[i].crawl_time     = results[i].get("crawl_time");
-				crawlData[i].createdAt      = results[i].get("createdAt");
+				var myBook = results[0].get("book");
+			
+				var payLoad = {};
+				var crawlData = [];
+				payLoad.title = myBook.get("title");
+				payLoad.author = myBook.get("author");
+				for(var i = 0; i < results.length; i++)
+				{
+					crawlData[i] = {};
+					crawlData[i].asin           = results[i].get("asin");
+					crawlData[i].sales_rank     = results[i].get("sales_rank");
+					crawlData[i].average_stars  = results[i].get("average_stars");
+					crawlData[i].kindle_price   = results[i].get("kindle_price");
+					crawlData[i].num_of_reviews = results[i].get("num_of_reviews");				
+					crawlData[i].crawl_date     = results[i].get("crawl_date");
+					crawlData[i].createdAt      = results[i].get("createdAt");
+				}
+				payLoad.crawl = crawlData.reverse();
 			}
-			payLoad.crawl = crawlData.reverse();
 			response.success(payLoad);
 		},
 		error: function()
