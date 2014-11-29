cd0a28b8dba25beda23ff158581d02520c5670d6
Andy Roberts <andy@note15.com>
2014-11-28 22:16:41 -0800

    Add Apple and Nook Sales
    New parse cloud code functions.
    
    Signed-off-by: Andy Roberts <andy@note15.com>

--- cloud/sales.js
+++ cloud/sales.js
@@ -7,6 +7,7 @@ Parse.Cloud.define("getSalesDataForAsin", function(request, response){
     query.equalTo("country","US");
     query.include("book");
     query.descending("crawlDate");
+    query.limit(999);
     if(request.params.skip)
     {
         query.skip(request.params.skip)
@@ -45,4 +46,106 @@ Parse.Cloud.define("getSalesDataForAsin", function(request, response){
         }
     });
  
+});
+
+Parse.Cloud.define("getSalesDataForApple", function(request, response){
+	var query = new Parse.Query("AggregateSales");
+  query.limit(999);
+  var Book = Parse.Object.extend("Book")
+  var book = new Book();
+
+  book.id = request.params.book;
+//  book.id = "S5attdRKzW";
+//  book.id = "K0lzhELnzG";
+  query.equalTo("book", book);
+  query.descending("crawlDate");
+
+  query.find({
+	success: function(results)
+	{
+		var payLoad = {};
+		payLoad.size = results.length;
+
+		var crawlData = [];
+		payLoad.crawl = crawlData;
+		if(results[0] != null)
+		{
+			var myBook = results[0].get("book");
+		  
+			var payLoad = {};
+			var crawlData = [];
+			payLoad.title = (myBook.id != null) ? myBook.get("title") : "";
+			payLoad.author = (myBook.id != null) ? myBook.get("author") : "";
+			j = 0;
+			for(var i = 0; i < results.length; i++)
+			{
+				if(results[i].get("appleSales") !=null)
+				{
+					crawlData[j] = {};
+					crawlData[j].objectId   = results[i].get("objectId");
+					crawlData[j].appleSales = results[i].get("appleSales");
+					crawlData[j].crawlDate  = results[i].get("crawlDate"); 
+					j++;
+				}
+			}	
+                	payLoad.crawl = crawlData.reverse();
+           	}	
+            	response.success(payLoad);
+        },
+        error: function()
+        {
+            response.error("lookup failed");
+        }
+    });
+});
+
+Parse.Cloud.define("getSalesDataForNook", function(request, response){
+	var query = new Parse.Query("AggregateSales");
+  query.limit(999);
+  var Book = Parse.Object.extend("Book")
+  var book = new Book();
+
+  book.id = request.params.book;
+//  book.id = "S5attdRKzW";
+//  book.id = "K0lzhELnzG";
+  query.equalTo("book", book);
+  query.descending("crawlDate");
+
+  query.find({
+	success: function(results)
+	{
+		var payLoad = {};
+		payLoad.size = results.length;
+
+		var crawlData = [];
+		payLoad.crawl = crawlData;
+		if(results[0] != null)
+		{
+			var myBook = results[0].get("book");
+		  
+			var payLoad = {};
+			var crawlData = [];
+			payLoad.title = (myBook.id != null) ? myBook.get("title") : "";
+			payLoad.author = (myBook.id != null) ? myBook.get("author") : "";
+			j = 0;
+			for(var i = 0; i < results.length; i++)
+			{
+				if(results[i].get("nookSales") !=null)
+				{
+					crawlData[j] = {};
+					crawlData[j].objectId   = results[i].get("objectId");
+					crawlData[j].nookSales = results[i].get("nookSales");
+					crawlData[j].crawlDate  = results[i].get("crawlDate"); 
+					j++;
+				}
+			}	
+                	payLoad.crawl = crawlData.reverse();
+           	}	
+            	response.success(payLoad);
+        },
+        error: function()
+        {
+            response.error("lookup failed");
+        }
+    });
 });
