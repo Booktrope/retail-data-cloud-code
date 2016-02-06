fd8392e87ef5a34da4c781f3002e34285bf17a41
Justin Jeffress <justin.jeffress@booktrope.com>
2016-02-05 18:00:23 -0800

    Convert Indent to spaces
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- cloud/promo.js
+++ cloud/promo.js
@@ -5,49 +5,49 @@
 // - Averaging 3.5 stars or more
 
 Parse.Cloud.define("getPromoReady", function(request, response){
-	var _ = require('underscore');
-	var payload = [];
-	var query = new Parse.Query("Book");
-	query.exists("asin");
-	query.limit(1000); //todo get books in batches once we publish more than 999
-	
-	query.find().then(function(results)
-	{
-		var promises = [];
-		_.each(results, function(result)
-		{
-			var crawlQuery = new Parse.Query("AmazonStats");
-			crawlQuery.equalTo("asin", result.get("asin"));
-			crawlQuery.greaterThan("kindle_price", 2.98);
-			crawlQuery.greaterThan("num_of_reviews",9);
-			crawlQuery.greaterThan("average_stars",3.4);
-			crawlQuery.limit(1);
-			crawlQuery.include("book");
-			promises.push(crawlQuery.first().then(function(crawlResult)
-			{ 
-				if(crawlResult != null) 
-				{ 
-					crawlData = {};
-					 var myBook = crawlResult.get("book");
-					crawlData.title = myBook.get("title");
-					crawlData.author = myBook.get("author");
-					crawlData.asin  = crawlResult.get("asin");
-					crawlData.kindle_price  	= crawlResult.get("kindle_price");
-					crawlData.average_stars 	= crawlResult.get("average_stars");
-					crawlData.num_of_reviews = crawlResult.get("num_of_reviews");
-					crawlData.crawl_date   	= crawlResult.get("crawl_date");
+  var _ = require('underscore');
+  var payload = [];
+  var query = new Parse.Query("Book");
+  query.exists("asin");
+  query.limit(1000); //todo get books in batches once we publish more than 999
 
-					payload.push(crawlData); 
+  query.find().then(function(results)
+  {
+    var promises = [];
+    _.each(results, function(result)
+    {
+      var crawlQuery = new Parse.Query("AmazonStats");
+      crawlQuery.equalTo("asin", result.get("asin"));
+      crawlQuery.greaterThan("kindle_price", 2.98);
+      crawlQuery.greaterThan("num_of_reviews",9);
+      crawlQuery.greaterThan("average_stars",3.4);
+      crawlQuery.limit(1);
+      crawlQuery.include("book");
+      promises.push(crawlQuery.first().then(function(crawlResult)
+      {
+        if(crawlResult != null)
+        {
+          crawlData = {};
+           var myBook = crawlResult.get("book");
+          crawlData.title = myBook.get("title");
+          crawlData.author = myBook.get("author");
+          crawlData.asin  = crawlResult.get("asin");
+          crawlData.kindle_price    = crawlResult.get("kindle_price");
+          crawlData.average_stars   = crawlResult.get("average_stars");
+          crawlData.num_of_reviews = crawlResult.get("num_of_reviews");
+          crawlData.crawl_date    = crawlResult.get("crawl_date");
 
-				}
-			}));
-		});
-		return Parse.Promise.when(promises);
+          payload.push(crawlData);
 
-	}).then(function(result)
-	{
-		response.success(payload);
+        }
+      }));
+    });
+    return Parse.Promise.when(promises);
 
-	});
- 
+  }).then(function(result)
+  {
+    response.success(payload);
+
+  });
+
 });
--- cloud/saveTriggers.js
+++ cloud/saveTriggers.js
@@ -3,168 +3,168 @@
 
 function sendPriceChangePush(request)
 {
-	var statusCode = request.object.get("status");
-	var channelName = request.object.get("channelName");
-	var title = request.object.get("title");
-	var price = request.object.get("price");
-	var alertMessage = "";
-	switch(statusCode)
-	{
-		case 0:
-			alertMessage = title + " was added to the queue for " + channelName + " and the price will be changed to $" +  price +".";
-			break;
-		case 25:
-			alertMessage = "Attempted to set the price of " + title + " to $" + price + " on " + channelName + ".";
-			break;
-		case 50:
-			alertMessage = "Price for " + title + " was set to $" + price + " on " + channelName + " waiting for confirmation.";
-			break;
-		case 99:
-			alertMessage = "Price change confirmed: " + title + " was set to $" + price + " on " + channelName + ".";
-			break;
-		case 404:
-			alertMessage = "Not Found: " + title + " was not found on " + channelName;
-			break;
-	}
+  var statusCode = request.object.get("status");
+  var channelName = request.object.get("channelName");
+  var title = request.object.get("title");
+  var price = request.object.get("price");
+  var alertMessage = "";
+  switch(statusCode)
+  {
+    case 0:
+      alertMessage = title + " was added to the queue for " + channelName + " and the price will be changed to $" +  price +".";
+      break;
+    case 25:
+      alertMessage = "Attempted to set the price of " + title + " to $" + price + " on " + channelName + ".";
+      break;
+    case 50:
+      alertMessage = "Price for " + title + " was set to $" + price + " on " + channelName + " waiting for confirmation.";
+      break;
+    case 99:
+      alertMessage = "Price change confirmed: " + title + " was set to $" + price + " on " + channelName + ".";
+      break;
+    case 404:
+      alertMessage = "Not Found: " + title + " was not found on " + channelName;
+      break;
+  }
 
-	Parse.Push.send(
-	{
-		channels: [ "PriceChanges" ],
-		data:
-		{
-			alert: alertMessage
-		}
-	},
-	{
-		success: function()
-		{
-    	// Push was successful
-  		},
-		error: function(error)
-		{
-		// Handle error
-		}
-	});
+  Parse.Push.send(
+  {
+    channels: [ "PriceChanges" ],
+    data:
+    {
+      alert: alertMessage
+    }
+  },
+  {
+    success: function()
+    {
+      // Push was successful
+      },
+    error: function(error)
+    {
+    // Handle error
+    }
+  });
 }
 
 Parse.Cloud.afterSave("PriceChangeQueue", function(request)
 {
-	sendPriceChangePush(request);
+  sendPriceChangePush(request);
 });
 
 Parse.Cloud.afterSave("PrefunkQueue", function(request)
 {
-	sendPriceChangePush(request);
+  sendPriceChangePush(request);
 });
 
 // the afterSave hook for AmazonSalesData
 Parse.Cloud.afterSave("AmazonSalesData", function(request)
 {
-	aggregateSales("amazonSales", request);
+  aggregateSales("amazonSales", request);
 });
 
 // the afterSave hook for AppleSalesData
 Parse.Cloud.afterSave("AppleSalesData", function(request)
 {
-	aggregateSales("appleSales", request);
+  aggregateSales("appleSales", request);
 });
 
 // the afterSave hook for NookSalesData
 Parse.Cloud.afterSave("NookSalesData", function(request)
 {
-	aggregateSales("nookSales", request);
+  aggregateSales("nookSales", request);
 });
 
 Parse.Cloud.afterSave("GooglePlaySalesData", function(request)
 {
-	aggregateSales("googleplaySales", request);
+  aggregateSales("googleplaySales", request);
 });
 
 //Base function for aggregating daily sales data into the AggregateSales class.
 function aggregateSales(salesFieldName, request)
 {
-	if(request.object.get("book") == null) { return; }
+  if(request.object.get("book") == null) { return; }
 
-	query = new Parse.Query("AggregateSales");
-	var Book = Parse.Object.extend("Book")
-	var book = new Book();
+  query = new Parse.Query("AggregateSales");
+  var Book = Parse.Object.extend("Book")
+  var book = new Book();
 
-	date = request.object.get("crawlDate");
-	searchDate = new Date(date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "Z00:00:00.000");
+  date = request.object.get("crawlDate");
+  searchDate = new Date(date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "Z00:00:00.000");
 
-	book.id = request.object.get("book").id;
-	query.equalTo("book", book);
-	query.equalTo("crawlDate", searchDate);
-	query.first(
-	{
-		success: function(aggregate)
-		{
-			// first returns success if there's no error and the object if one exists
-			// otherwise it's null, so we check for not null and update, otherwise we create new.
-			if(aggregate == null)
-			{
-				var Aggregate = Parse.Object.extend("AggregateSales");
-				aggregate = new Aggregate();
-				aggregate.set("crawlDate", searchDate);
-			}
-			prevTotal = aggregate.get(salesFieldName) != null ? aggregate.get(salesFieldName) : 0;
+  book.id = request.object.get("book").id;
+  query.equalTo("book", book);
+  query.equalTo("crawlDate", searchDate);
+  query.first(
+  {
+    success: function(aggregate)
+    {
+      // first returns success if there's no error and the object if one exists
+      // otherwise it's null, so we check for not null and update, otherwise we create new.
+      if(aggregate == null)
+      {
+        var Aggregate = Parse.Object.extend("AggregateSales");
+        aggregate = new Aggregate();
+        aggregate.set("crawlDate", searchDate);
+      }
+      prevTotal = aggregate.get(salesFieldName) != null ? aggregate.get(salesFieldName) : 0;
 
-			aggregate.set("book", book);
-			aggregate.set(salesFieldName, prevTotal + request.object.get("dailySales"));
+      aggregate.set("book", book);
+      aggregate.set(salesFieldName, prevTotal + request.object.get("dailySales"));
 
-			aggregate.save();
-		},
-		error: function(error)
-		{
-			console.error("Got an error " + error.code + " : " + error.message);
-		}
-	});
+      aggregate.save();
+    },
+    error: function(error)
+    {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
 }
 
 
 // the afterSave hook for NookStats
 Parse.Cloud.afterSave("NookStats", function(request)
 {
-	saveIntoScoreBoard("NookScoreBoard", ["salesRank","price","reviewCount", "averageCount", "crawlDate"], request);
+  saveIntoScoreBoard("NookScoreBoard", ["salesRank","price","reviewCount", "averageCount", "crawlDate"], request);
 });
 
 Parse.Cloud.afterDelete("NookStats", function(request)
 {
-	deleteFromScoreBoard("NookScoreBoard", "NookStats", request);
+  deleteFromScoreBoard("NookScoreBoard", "NookStats", request);
 });
 
 
 //afterSave hook for AmazonStats
 Parse.Cloud.afterSave("AmazonStats", function(request)
 {
-	saveIntoScoreBoard("AmazonScoreBoard", ["got_price", "kindle_price", "sales_rank", "crawl_date", "num_of_reviews", "average_stars"], request);
+  saveIntoScoreBoard("AmazonScoreBoard", ["got_price", "kindle_price", "sales_rank", "crawl_date", "num_of_reviews", "average_stars"], request);
 });
 
 Parse.Cloud.afterDelete("AmazonStats", function(request)
 {
-	deleteFromScoreBoard("AmazonScoreBoard", "AmazonStats", request);
+  deleteFromScoreBoard("AmazonScoreBoard", "AmazonStats", request);
 });
 
 //afterSave hook for AppleStats
 Parse.Cloud.afterSave("AppleStats", function(request)
 {
-	saveIntoScoreBoard("AppleScoreBoard", ["price", "numOfReviews", "averageStars", "crawlDate"], request);
+  saveIntoScoreBoard("AppleScoreBoard", ["price", "numOfReviews", "averageStars", "crawlDate"], request);
 });
 
 Parse.Cloud.afterDelete("AppleStats", function(request)
 {
-	deleteFromScoreBoard("AppleScoreBoard", "AppleStats", request);
+  deleteFromScoreBoard("AppleScoreBoard", "AppleStats", request);
 });
 
 //afterSave hook for GooglePlayStats
 Parse.Cloud.afterSave("GooglePlayStats", function(request)
 {
-	saveIntoScoreBoard("GooglePlayScoreBoard", ["price", "numOfReviews", "averageReviews", "crawlDate"], request);
+  saveIntoScoreBoard("GooglePlayScoreBoard", ["price", "numOfReviews", "averageReviews", "crawlDate"], request);
 });
 
 Parse.Cloud.afterDelete("GooglePlayStats", function(request)
 {
-	deleteFromScoreBoard("GooglePlayScoreBoard", "GooglePlayStats", request);
+  deleteFromScoreBoard("GooglePlayScoreBoard", "GooglePlayStats", request);
 });
 
 // base function for saving stats into a scoreboard.
@@ -174,61 +174,61 @@ Parse.Cloud.afterDelete("GooglePlayStats", function(request)
 // for the Book, but either updating or creating a new record if one doesn't exist.
 function saveIntoScoreBoard(scoreBoard, fields, request)
 {
-	query = new Parse.Query(scoreBoard);
-	var Book = Parse.Object.extend("Book")
-	var book = new Book();
+  query = new Parse.Query(scoreBoard);
+  var Book = Parse.Object.extend("Book")
+  var book = new Book();
 
-	book.id = request.object.get("book").id;
-	query.equalTo("book", book);
-	query.include("stats");
-	query.first(
-	{
-		success: function(score)
-		{
-			// first returns success if there's no error and the object if one exists
-			// otherwise it's null, so we check for not null and update, otherwise we create new.
-			if(score == null)
-			{
-				var Score = Parse.Object.extend(scoreBoard);
-				score = new Score();
-			}
-			score.set("book", book);
-			score.set("stats",request.object);
-			for(var i = 0; i < fields.length; i++)
-			{
-				score.set(fields[i], score.get("stats").get(fields[i]));
-			}
-			score.save();
-		},
-		error: function(error)
-		{
-			console.error("Got an error " + error.code + " : " + error.message);
-		}
-	});
+  book.id = request.object.get("book").id;
+  query.equalTo("book", book);
+  query.include("stats");
+  query.first(
+  {
+    success: function(score)
+    {
+      // first returns success if there's no error and the object if one exists
+      // otherwise it's null, so we check for not null and update, otherwise we create new.
+      if(score == null)
+      {
+        var Score = Parse.Object.extend(scoreBoard);
+        score = new Score();
+      }
+      score.set("book", book);
+      score.set("stats",request.object);
+      for(var i = 0; i < fields.length; i++)
+      {
+        score.set(fields[i], score.get("stats").get(fields[i]));
+      }
+      score.save();
+    },
+    error: function(error)
+    {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
 }
 
 //base function for deleting stats from the scoreboard.
 //if the stat is deleted, we delete it from the scoreboard.
 function deleteFromScoreBoard(scoreBoard, stats, request)
 {
-	query = new Parse.Query(scoreBoard);
+  query = new Parse.Query(scoreBoard);
 
-	var Stats = Parse.Object.extend(stats)
-	var stats = new Stats();
-	stats.id = request.object.id;
+  var Stats = Parse.Object.extend(stats)
+  var stats = new Stats();
+  stats.id = request.object.id;
 
-	query.equalTo("stats", stats);
-	query.first({
-		success: function(stat)
-		{
-			if(stat != null)
-			{
-				stat.destroy();
-			}
-		},
-		error: function(error)
-		{
-			console.error("Got an error " + error.code + " : " + error.message);
-		}
-	});
+  query.equalTo("stats", stats);
+  query.first({
+    success: function(stat)
+    {
+      if(stat != null)
+      {
+        stat.destroy();
+      }
+    },
+    error: function(error)
+    {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
 }
